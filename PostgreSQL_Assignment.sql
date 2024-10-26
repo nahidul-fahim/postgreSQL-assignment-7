cb1dcac9fb079339bd6fe312c135f0936550ec40
nahidul-fahim <nahidul7562@gmail.com>
2024-10-26 12:20:13 +0600

    created database, tables and inserted sample data into the tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -3,10 +3,111 @@
 -- create students table
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,
-    student_name VARCHAR(200),
-    age INT,
-    email VARCHAR(255) NOT null,
+    student_name VARCHAR(200) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
     frontend_mark INT NOT NULL,
     backend_mark INT NOT NULL,
     status VARCHAR(50)
 );
+
+-- create courses table
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    credits INT NOT NULL
+);
+
+-- create an enrollment table
+CREATE TABLE enrollment (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INT REFERENCES students (student_id),
+    course_id INT REFERENCES courses (course_id)
+);
+
+-- insert data into the students table
+INSERT INTO
+    students (
+        student_name,
+        age,
+        email,
+        frontend_mark,
+        backend_mark
+    )
+VALUES (
+        'Sameer',
+        21,
+        'sameer@example.com',
+        48,
+        60
+    ),
+    (
+        'Zoya',
+        23,
+        'zoya@example.com',
+        52,
+        58
+    ),
+    (
+        'Nabil',
+        22,
+        'nabil@example.com',
+        37,
+        46
+    ),
+    (
+        'Rafi',
+        24,
+        'rafi@example.com',
+        41,
+        40
+    ),
+    (
+        'Sophia',
+        22,
+        'sophia@example.com',
+        50,
+        52
+    ),
+    (
+        'Hasan',
+        23,
+        'hasan@gmail.com',
+        43,
+        39
+    );
+
+-- insert data into courses table
+INSERT INTO
+    courses (course_name, credits)
+VALUES ('Next.js', 3),
+    ('React.js', 4),
+    ('Databases', 3),
+    ('Prisma', 3);
+
+-- insert data into enrollment table
+INSERT INTO
+    enrollment (student_id, course_id)
+VALUES (1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 2);
+
+-- insert new student record in the students table
+INSERT INTO
+    students (
+        student_name,
+        age,
+        email,
+        frontend_mark,
+        backend_mark,
+        status
+    )
+VALUES (
+        'Nahid',
+        25,
+        'nahid@example.com',
+        60,
+        55,
+        NULL
+    );
